dc178f985e26acbc98548e7de8f783e426e87003
LeoLJ <ljliu@cycu.org.tw>
2016-05-27 15:58:52 +0800

    Add new practice AcDemoApp

--- ACDemoApp/AboutAC.swift
+++ ACDemoApp/AboutAC.swift
@@ -10,9 +10,23 @@ import UIKit
 
 class AboutAC: UIViewController {
 
+    @IBOutlet weak var labelA: UILabel!
+    
+    @IBOutlet weak var labelB: UILabel!
+    
+    @IBAction func buttonA(sender: AnyObject) {
+    }
+    
+    @IBOutlet weak var labelC: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.labelA.text = String("全台第一所網路新創事業學校")
+        self.labelB.text = String("由國際網路業界菁英領軍")
+        self.labelC.text = String("無私指導，讓你完整掌握業界網路行銷與創業秘訣")
+        
         // Do any additional setup after loading the view.
     }
 
